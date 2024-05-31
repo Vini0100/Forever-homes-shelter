3d63c9b08412464ffdafbc365f557c7d45ced555
Vinícius Gonzato <vinigonzatofw@gmail.com>
2024-05-31 14:26:36 -0300

    Added Donate Modal, including styles and scripts. Adjusted buttons to have cursor pointer.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Start
 
 passAplyAdopt()
+passDonate()
 
 // Modal
 
@@ -14,6 +15,22 @@ function passAplyAdopt() {
             modalApplyAdopt.showModal();
         });
     }    
+}
+
+function passDonate() {
+    const modalDonate = document.getElementById("donate-modal");
+    const btDonate = document.getElementsByClassName("donate-button");
+
+    for (let i = 0; i < btDonate.length; i++) {
+        btDonate[i].addEventListener('click', function() {
+            modalDonate.showModal();
+        });
+    }    
+}
+
+function closeModal(modal) {
+    const modalClose = document.getElementById(modal);
+    modalClose.close();
 }
 
 // Birth
@@ -57,3 +74,41 @@ function populateYears() {
         yearSelect.appendChild(option);
     }
 }
+
+// Money
+
+const moneyInput = document.getElementById('money');
+
+moneyInput.addEventListener('blur', function() {
+    let value = moneyInput.value.trim();
+
+    if (value.startsWith('R$')) {
+        value = value.slice(2).trim();
+    }
+    value = value.replace(',', '.');
+
+    const isNumber = !isNaN(value) && value !== '';
+    if (isNumber) {
+        moneyInput.value = `R$ ${parseFloat(value).toFixed(2)}`;
+    } else {
+        moneyInput.value = '';
+    }
+});
+
+// Event Payment button
+
+document.addEventListener('DOMContentLoaded', () => {
+    const radios = document.querySelectorAll('.custom-check');
+
+    radios.forEach(radio => {
+        radio.addEventListener('change', () => {
+            const paymentItems = document.querySelectorAll('.payment-item');
+            paymentItems.forEach(item => {
+                item.classList.remove('checked');
+            });
+            if (radio.checked) {
+                radio.closest('.payment-item').classList.add('checked');
+            }
+        });
+    });
+});
